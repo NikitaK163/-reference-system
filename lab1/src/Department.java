3978ef5cffd1335b9c95985aa8639a0d3235c289
NikitaK163 <stl1995163@mail.ru>
2016-11-13 21:21:34 +0300

    reference-system

---
+++ lab1/src/Department.java
@@ -0,0 +1,7 @@
+/**
+ * Created by Вова on 13.11.2016.
+ */
+import java.lang.String;
+public class Department {
+    public String name,chief;
+}
---
+++ lab1/src/employe.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Никита on 13.11.2016.
+ */
+public class employe {
+
+
+    public String fio, tel, sal, chief;
+    Department dep;
+}
